0d9621ff159593df9f0cf4120354ebe8213b2e29
Darus Slah <darusslah@gmail.com>
2023-06-17 12:46:55 -0400

    first commit

---
+++ src/test/java/C13Group2/BankingAPI/BankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package C13Group2.BankingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
